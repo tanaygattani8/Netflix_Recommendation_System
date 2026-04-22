c995f2c1e4710b388da11a83554d19bd20d1461e
tanaygattani8 <tanaygattani8@gmail.com>
2026-04-21 19:53:20 -0500

    Replace API key with placeholder text

--- static/recommend.js
+++ static/recommend.js
@@ -12,7 +12,7 @@ $(function () {
   source.addEventListener('input', inputHandler);
 
   $('.movie-button').on('click', function () {
-    var my_api_key = 'aa449cf1f3342eb70dc0e485d2372de6';
+    var my_api_key = 'Enter Your API from tmdb';
     var title = $('.movie').val();
     if (title == "") {
       $('.results').css('display', 'none');
@@ -26,7 +26,7 @@ $(function () {
 
 // will be invoked when clicking on the recommended movies
 function recommendcard(e) {
-  var my_api_key = 'aa449cf1f3342eb70dc0e485d2372de6';
+  var my_api_key = 'Enter Your API from tmdb';
   var title = e.getAttribute('title');
   load_details(my_api_key, title);
 }
